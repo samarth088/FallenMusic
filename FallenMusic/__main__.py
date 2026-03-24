b2f3e107f8c75beab98bd61fc89b47a9ffd1c94c
samarth088 <skshah987654@gmail.com>
2026-03-24 13:09:12 +0530

    Add HTTP server functionality with request handler
    Signed-off-by: samarth088 <skshah987654@gmail.com>

--- FallenMusic/__main__.py
+++ FallenMusic/__main__.py
@@ -58,6 +58,23 @@ threading.Thread(
     daemon=True
 ).start()
 
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'OK')
+
+    def log_message(self, format, *args):
+        return
+
+def run_server():
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server, daemon=True).start()
 
 async def fallen_startup():
     LOGGER.info("[•] Loading Modules...")
